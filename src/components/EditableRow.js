0766cbbed3cb1076565d926ee882c11020872edc
warepradip <warepradip9@gmail.com>
2022-05-04 17:26:58 +0530

    firstcomment

---
+++ src/components/EditableRow.js
@@ -0,0 +1,49 @@
+import React from "react";
+const EditableRow = ({
+  editFormData,
+  handleEditFormChange,
+  handleCancelClick,
+}) => {
+  return (
+    <tr>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter a User Name..."
+          name="fullName"
+          value={editFormData.fullName}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter Your Phone number..."
+          name="phoneNumber"
+          value={editFormData.phoneNumber}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter Your Email..."
+          name="email"
+          value={editFormData.email}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <button type="submit">Save</button>
+        <button type="button" onClick={handleCancelClick}>
+          Cancle
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default EditableRow;
